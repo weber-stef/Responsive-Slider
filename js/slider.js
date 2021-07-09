97e28ef7d0b0ccf265cd21176e7c3ac4975ef7ee
weber-stef <vitatech@gmx.de>
2021-07-09 17:16:03 +0200

    added subfolders

--- js/slider.js
+++ js/slider.js
@@ -1,54 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./css/styles.css">
-</head>
-
-<body>
-
-    <div class="slideshow-container">
-
-        <div class="mySlides fade">
-            <div class="numbertext"></div>
-            <img src="./img/slide1.jpg" style="width:100%">
-            <div class="text"></div>
-        </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext"></div>
-            <img src="./img/slide2.jpg" style="width:100%">
-            <div class="text"></div>
-        </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext"></div>
-            <img src="./img/slide3.jpg" style="width:100%">
-            <div class="text"></div>
-        </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext"></div>
-            <img src="./img/slide4.jpg" style="width:100%">
-            <div class="text"></div>
-        </div>
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-
-
-    </div>
-    <br>
-
-
-    <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-        <span class="dot" onclick="currentSlide(4)"></span>
-    </div>
-
-    <script>
         var slideIndex = 1;
         var fadetime = 10000;
         autoSlide();
@@ -105,8 +54,3 @@
             dots[slideIndex - 1].className += " active";
 
         }
-    </script>
-
-</body>
-
-</html>
